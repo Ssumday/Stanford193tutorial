92f0a6f3adfcc51328e9c0491b2df67045435648
Kevin3272 <ktrang3@calstatela.edu>
2020-06-29 18:36:02 -0700

    End of Lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,9 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, There World!")
+         HStack{ForEach(0..<4){ index in
+            CardView(isFaceUp: false)
+        }
+         .padding()
+         .foregroundColor(Color.orange)
+         .font(Font.largeTitle)
     }//end of body
 }//end of struct Content View
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack{
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else{
+            RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+            }
+           
+        }
+    }
+}
 
 //Struct container for variables, functions, behaviors
 //name is ContentView
@@ -26,7 +48,14 @@ struct ContentView: View {
 //The value of body is executed. Basically it runs everytime and then it returns the one liner. If it is a one liner you can just leave return out.
 //Text behaves like a View so we can just return the text as the body
 
-//Swift is strongly typed language, just like Java 
+//Swift is strongly typed language, just like Java
+//CornerRadius is a label for the argument 10.0
+
+//ZStack retuns some view that are stacked in order
+//Stroke highlights the thingy you call the function on. It returns a view
+//foreground color can change the color of any view
+//You can even run it on ZStack. It will tell every view inside of the ZStack to make everything Orange
+//You can override it from the inside
 
 
 
