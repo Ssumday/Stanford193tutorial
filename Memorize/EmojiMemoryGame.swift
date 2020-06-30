9295d1ef99af6ecbb5eb43838c3bc2427497bc0e
Kevin3272 <ktrang3@calstatela.edu>
2020-06-30 08:55:37 -0700

    Model View Start

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Kevin Trang on 6/30/20.
+//  Copyright © 2020 Kevin Trang. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame{
+   private(set) var model: MemoryGame<String>
+}//end of class
